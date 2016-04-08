63a8d7fb247f926cfca41e83fa1989dd3014fc36
Ricardo <riboher@gmail.com>
2016-04-08 11:28:47 +0200

    Added Knight class

---
+++ lib/Knight.rb
@@ -0,0 +1,12 @@
+require './Modules.rb'
+require './Piece.rb'
+
+class Knight < Piece
+  def initialize(origin,color)
+    super(origin,color)
+  end
+
+  def check_move(target_position)
+
+  end
+end
