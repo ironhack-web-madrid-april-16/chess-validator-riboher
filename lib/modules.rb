9b1aef321718bd52aea4ae362534442146a2fba8
Ricardo <riboher@gmail.com>
2016-04-08 11:27:35 +0200

    Added king movement

--- lib/modules.rb
+++ lib/modules.rb
@@ -28,8 +28,14 @@ end
 
 module Linear_movable_by_one
   def move_one_linear(origin,target_position)
-    if @origin[0]+1 == target_position[0] || @origin[1]+1 == target_position[1]
-      @movable = true
+    if @origin[0] == target_position[0]
+      if target_position[1] == @origin[1] + 1 || target_position[1] == @origin[1] - 1
+        @movable = true
+      end
+    elsif @origin[1] == target_position[1]
+      if target_position[0] == @origin[0] + 1 || target_position[0] == @origin[0] - 1
+        @movable = true
+      end
     end
     @movable
   end
