4836a5f73ad327fe9fde7122eab51ff456ba09a6
Ricardo <riboher@gmail.com>
2016-04-08 16:02:59 +0200

    Tested board check position

--- lib/chess_validator.rb
+++ lib/chess_validator.rb
@@ -5,18 +5,16 @@ require './Bishop.rb'
 require './King.rb'
 require './Knight.rb'
 require './Pawn.rb'
+require './Board.rb'
+require 'pry'
 
-# chessboard = [
-#   [nil,nil,nil,nil,nil,nil,nil,nil],
-#   [nil,nil,nil,nil,nil,nil,nil,nil],
-#   [nil,nil,nil,nil,nil,nil,nil,nil],
-#   [nil,nil,nil,nil,nil,nil,nil,nil],
-#   [nil,nil,nil,nil,nil,nil,nil,nil],
-#   [nil,nil,nil,nil,nil,nil,nil,nil],
-#   [nil,nil,nil,nil,nil,nil,nil,nil],
-#   [nil,nil,nil,nil,nil,nil,nil,nil]
-# ]
 
+chessboard = Board.new
+chessboard.fill_grid("simple-board.txt")
+chessboard.check_position([0,2])
+
+# moves = IO.read("simple-moves.txt").split("\n")
+# chessboard = chessboard.map { |piece| piece.to_sym }
 
 
 
@@ -25,24 +23,13 @@ require './Pawn.rb'
 # wQ = Queen.new([4,7],"white")
 # wK = King.new([3,4],"white")
 # wN = Knight.new([3,4],"white")
-wP1 = Pawn.new([0,6],"white")
-wP2 = Pawn.new([1,6],"white")
-wP3 = Pawn.new([2,6],"white")
-wP4 = Pawn.new([3,6],"white")
-wP5 = Pawn.new([4,6],"white")
-wP6 = Pawn.new([5,6],"white")
-wP7 = Pawn.new([6,6],"white")
-wP8 = Pawn.new([7,6],"black")
+# wP1 = Pawn.new([0,6],"white")
+# wP2 = Pawn.new([1,6],"white")
+# wP3 = Pawn.new([2,6],"white")
+# wP4 = Pawn.new([3,6],"white")
+# wP5 = Pawn.new([4,6],"white")
+# wP6 = Pawn.new([5,6],"white")
+# wP7 = Pawn.new([6,6],"white")
+# wP8 = Pawn.new([7,6],"black")
 
-# wR.check_move([5,0])
-# wB.check_move([5,3])
-# wQ.check_move([0,3])
-# wN.check_move([4,2])
-# wN.check_move([5,3])
-# wN.check_move([5,5])
-# wN.check_move([4,6])
-# wN.check_move([2,6])
-# wN.check_move([1,5])
-# wN.check_move([1,3])
-# wN.check_move([2,2])
 
