80673504ee0aade5d49830d817bf308872b36825
Ricardo <riboher@gmail.com>
2016-04-08 11:27:49 +0200

    Tested King movement

--- lib/chess_validator.rb
+++ lib/chess_validator.rb
@@ -2,6 +2,7 @@ require './modules.rb'
 require './Queen.rb'
 require './Rook.rb'
 require './Bishop.rb'
+require './King.rb'
 
 chessboard = [
   [nil,nil,nil,nil,nil,nil,nil,nil],
@@ -17,12 +18,15 @@ chessboard = [
 
 
 
-wR = Rook.new([0,0],"white")
-wB = Bishop.new([2,0],"white")
-wQ = Queen.new([4,7],"white")
+# wR = Rook.new([0,0],"white")
+# wB = Bishop.new([2,0],"white")
+# wQ = Queen.new([4,7],"white")
+wK = King.new([3,4],"white")
 #chessboard[0][0] = wR
 
-wR.check_move([5,0])
-wB.check_move([5,3])
-wQ.check_move([0,3])
+# wR.check_move([5,0])
+# wB.check_move([5,3])
+# wQ.check_move([0,3])
+
+wK.check_move([4,4])
 
