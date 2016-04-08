9984da970f585ae468475197a98db4a470298d94
Ricardo <riboher@gmail.com>
2016-04-08 12:56:10 +0200

    Added Pawn Test for blacks and whites.

--- lib/chess_validator.rb
+++ lib/chess_validator.rb
@@ -1,9 +1,10 @@
-require './Modules.rb'
+require './modules.rb'
 require './Queen.rb'
 require './Rook.rb'
 require './Bishop.rb'
 require './King.rb'
 require './Knight.rb'
+require './Pawn.rb'
 
 # chessboard = [
 #   [nil,nil,nil,nil,nil,nil,nil,nil],
@@ -24,9 +25,24 @@ require './Knight.rb'
 # wQ = Queen.new([4,7],"white")
 # wK = King.new([3,4],"white")
 # wN = Knight.new([3,4],"white")
+wP1 = Pawn.new([0,6],"white")
+wP2 = Pawn.new([1,6],"white")
+wP3 = Pawn.new([2,6],"white")
+wP4 = Pawn.new([3,6],"white")
+wP5 = Pawn.new([4,6],"white")
+wP6 = Pawn.new([5,6],"white")
+wP7 = Pawn.new([6,6],"white")
+wP8 = Pawn.new([7,6],"black")
 
-
-
-
-
+# wR.check_move([5,0])
+# wB.check_move([5,3])
+# wQ.check_move([0,3])
+# wN.check_move([4,2])
+# wN.check_move([5,3])
+# wN.check_move([5,5])
+# wN.check_move([4,6])
+# wN.check_move([2,6])
+# wN.check_move([1,5])
+# wN.check_move([1,3])
+# wN.check_move([2,2])
 
