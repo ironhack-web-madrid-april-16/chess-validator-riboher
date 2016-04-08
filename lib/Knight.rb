cdd368fd49144c824e1a7ef5f56c4680286a61f4
Ricardo <riboher@gmail.com>
2016-04-08 12:21:59 +0200

    Added Knight parameters

--- lib/Knight.rb
+++ lib/Knight.rb
@@ -2,11 +2,16 @@ require './Modules.rb'
 require './Piece.rb'
 
 class Knight < Piece
+  include L_movement
   def initialize(origin,color)
     super(origin,color)
   end
 
   def check_move(target_position)
-
+    if l_movable(@origin,target_position)
+      puts "LEGAL"
+    else
+      puts "ILLEGAL"
+    end
   end
 end
