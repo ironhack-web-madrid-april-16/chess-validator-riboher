bc8948296c98483397220d01415610963877791f
Ricardo <riboher@gmail.com>
2016-04-08 16:02:43 +0200

    Used ternary conditionals

--- lib/modules.rb
+++ lib/modules.rb
@@ -2,25 +2,21 @@ require 'pry'
 
 module Linear_movable
   def move_linear(target_position)
-    if @origin[0] == target_position[0] || @origin[1] == target_position[1]
-      @movable = true
-    end
-    @movable
+    @origin[0] == target_position[0] || @origin[1] == target_position[1] ? @movable = true : @movable
   end
 end
 
 module Diagonal_movable
    def move_diagonal(target_position)
     if @origin[0] > target_position[0] && @origin[1] < target_position[1]
-      if @origin[0] - target_position[0] == @origin[1] + target_position[1]
-        @movable = true
-      end
+
+      @origin[0] - target_position[0] == @origin[1] + target_position[1] ? @movable = true : @movable
+
     elsif @origin[0] > target_position[0] && @origin[1] > target_position[1] ||
           @origin[0] < target_position[0] && @origin[1] < target_position[1] ||
           @origin[0] < target_position[0] && @origin[1] > target_position[1]
-      if @origin[0] - target_position[0] == @origin[1] - target_position[1]
-        @movable = true
-      end
+
+          @origin[0] - target_position[0] == @origin[1] - target_position[1] ? @movable = true : @movable
     end
     @movable
   end
